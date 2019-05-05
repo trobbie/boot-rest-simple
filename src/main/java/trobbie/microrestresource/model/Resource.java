c032070014b9dbc7f03fae9191ddc85ef7f62679
Trevor Robbie <trobbie@gmail.com>
2019-05-05 16:55:25 -0400

    added getters/setters

--- src/main/java/trobbie/microrestresource/model/Resource.java
+++ src/main/java/trobbie/microrestresource/model/Resource.java
@@ -6,20 +6,45 @@ package trobbie.microrestresource.model;
  */
 public class Resource {
 
-	private final long id;
-	private final String name;
+	private long id;
+	private String name;
+
+	public Resource() {
+	}
 
 	public Resource(long id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
+	/**
+	 * @return the id
+	 */
 	public long getId() {
 		return id;
 	}
 
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 }
 
