40a5bc72397bbc01f0f9944306cdf42b5b45fc29
Trevor Robbie <trobbie@gmail.com>
2019-05-19 16:55:28 -0400

    made service layer use repository

--- src/main/java/trobbie/microrestresource/service/ResourceService.java
+++ src/main/java/trobbie/microrestresource/service/ResourceService.java
@@ -1,10 +1,11 @@
 package trobbie.microrestresource.service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 
+import trobbie.microrestresource.dao.ResourcesRepository;
 import trobbie.microrestresource.model.Resource;
 
 /**
@@ -16,39 +17,31 @@ import trobbie.microrestresource.model.Resource;
 @Component
 public class ResourceService {
 
-	private static List<Resource> allResources;
+	private ResourcesRepository resourcesRepository;
 
-	static {
-		// TODO: needs data layer implementation
-		//   For now, hard-code the data
-		allResources = new ArrayList<Resource>();
-		allResources.add(new Resource(1, "TestUser1"));
-	}
-
-	/*
+	/**
 	 * Returns list of resources. Returns empty list if no resources.
 	 */
 	public List<Resource> getResources() {
-		return allResources;
+		return resourcesRepository.findAll();
 	}
 
-	/*
+	/**
 	 * Returns resource object, given the id of the resource. Returns null if id not found.
 	 *
 	 * @param id the id of the resource
 	 * @return the retrieved resource object, or null if not found
 	 */
 	public Resource getResource(Long id) {
-		for (Resource r : allResources) {
-			if (r.getId() == id) {
-				return r;
-			}
-		}
-		return null;
+		Optional<Resource> result = resourcesRepository.findById(id);
+		if (result.isPresent())
+			return result.get();
+		else
+			return null;
 	}
 
-	/*
-	 * Replaces the resource object at the specified resource's id.  If resource id does not already
+	/**
+	 * Replaces the resource object at the specified resource's id.  If resource's id does not already
 	 * exist, then return null.
 	 *
 	 * <p> Note: this method must be idempotent.
@@ -58,14 +51,7 @@ public class ResourceService {
 	 * @return the replaced resource object, or null if id could not be found
 	 */
 	public Resource replaceResource(Resource specifiedResource) {
-
-		Resource resourceToUpdate = getResource(specifiedResource.getId());
-		if (resourceToUpdate == null) return null;
-
-		resourceToUpdate.setName(specifiedResource.getName());
-
-		return resourceToUpdate;
-
+		return resourcesRepository.save(specifiedResource);
 	}
 
 }
